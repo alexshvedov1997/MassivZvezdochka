cca32c849f8d4467200e5626a1fa03de84c07398
Alex Shvedov <User@DESKTOP-V2MV0KM>
2019-10-06 20:54:45 +0300

    MassivZvezdocka

---
+++ ConsOpMassZvezdochka/ClassMethods.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsOpMassZvezdochka
+{
+    class ClassMethods
+    {
+        static public int MaxElement(params int[] mass) {
+            int max = mass[0];
+            for (int i = 0; i < mass.Length; i++) {
+                if (max < mass[i]) max = mass[i];
+            }
+            return max;        
+        }
+
+        static public void ArrayPrint( char[,] mass) {
+            Console.WriteLine(" Результат");
+            for (int i = 0; i < mass.GetLength(0); i++, Console.WriteLine())
+            {
+                for (int j = 0; j < mass.GetLength(1); j++)
+                {
+                    Console.Write(mass[i,j]);
+                }
+            } 
+        }
+        static public char[,] ArrayFiling( params int [] mass) {
+            int n = ClassMethods.MaxElement(mass);
+            char[,] array = new char[n, mass.Length];
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        if (   i < array.GetLength(0) - mass[j]) array[i, j] = ' ';
+                        if (  i >= array.GetLength(0) - mass[j]) array[i, j] = '*';
+                    }
+                }
+                return array;
+            }
+
+        static public string ArrayToString(char[,] mass) {
+            string text = null;
+            for (int i = 0; i < mass.GetLength(0); i++, Console.WriteLine())
+            {
+                for (int j = 0; j < mass.GetLength(1); j++)
+                {
+                  text += mass[i, j];
+                }
+                  text += "\n";
+            }
+            return text;
+        
+        }
+        
+        
+    }
+}
---
+++ ConsOpMassZvezdochka/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ConsOpMassZvezdochka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = null;
+            string path = @"D:\programmC#\TextC#.txt";
+            string path2 = @"D:\programmC#\TextWriting.txt";
+             using(FileStream read = File.OpenRead(path)){     
+                 byte[] array = new byte[read.Length];
+              read.Read(array, 0, array.Length);
+            str = Encoding.Default.GetString(array);
+             }
+        int[] mass = str.Where(x => Char.IsDigit(x)).Select( j => (int)Char.GetNumericValue(j)).ToArray(); // чтение чисел из файла
+         char[,] arr = ClassMethods.ArrayFiling( mass);
+          ClassMethods.ArrayPrint(arr);
+          str = ClassMethods.ArrayToString(arr);
+          using (FileStream write = new FileStream(path2, FileMode.Append)) {
+              byte[] mas = Encoding.Default.GetBytes(str);
+              write.Write(mas, 0, mas.Length);
+          }
+        }
+    }
+}
